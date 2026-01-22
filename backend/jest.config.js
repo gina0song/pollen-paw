be956946f3ffd27c9a2421a99356990932802c18
Gina Song <gina0song@gmail.com>
2026-01-22 11:37:55 -0800

    chore: add Jest and testing dependencies

---
+++ backend/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests', '<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: {
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+      }
+    }]
+  },
+  testEnvironmentOptions: {
+    url: 'http://localhost'
+  },
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
